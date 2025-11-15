2bbdf96130ce97399843b3f8e191b29db6495a8a
tsssni <dingyongyu2002@foxmail.com>
2025-11-15 21:43:35 +0800

    Add nixos files for building pbrt-v4

---
+++ nix/default.nix
@@ -0,0 +1,86 @@
+{
+  addDriverRunpath,
+  cmake,
+  cudaPackages,
+  cudaSupport ? false,
+  fetchFromGitHub,
+  git,
+  glfw,
+  lib,
+  openexr,
+  pkg-config,
+  stdenv,
+  zlib,
+}:
+let
+  stdenv' = if cudaSupport then cudaPackages.backendStdenv else stdenv;
+  optix = fetchFromGitHub {
+    owner = "NVIDIA";
+    repo = "optix-dev";
+    tag = "v9.0.0";
+    hash = "sha256-WbMKgiM1b3IZ9eguRzsJSkdZJR/SMQTda2jEqkeOwok=";
+  };
+in
+stdenv'.mkDerivation {
+  pname = "pbrt-v4";
+  version = "2025-10-25";
+
+  src = fetchFromGitHub {
+    owner = "mmp";
+    repo = "pbrt-v4";
+    rev = "91bc6ca";
+    fetchSubmodules = true;
+    sha256 = "sha256-MUsn/JUe8scyD0/dv/mT9bu/A5pk21Kb0C5Af+5WWWI=";
+  };
+
+  patches = [
+    # use glfw in nixpkgs
+    ./glfw.patch
+    # fix unmatched __CUDA_ARCH__ https://github.com/mmp/pbrt-v4/issues/429
+    ./cuda-arch.patch
+    # set libcuda.so path
+    ./libcuda.patch
+  ];
+
+  nativeBuildInputs = [
+    cmake
+    git
+    pkg-config
+  ]
+  ++ lib.optionals cudaSupport [
+    addDriverRunpath
+    cudaPackages.cuda_nvcc
+  ];
+
+  buildInputs = [
+    glfw
+    openexr
+    zlib
+  ]
+  ++ lib.optionals cudaSupport (
+    with cudaPackages;
+    [
+      cuda_cccl
+      cuda_cudart
+    ]
+  );
+
+  cmakeFlags = lib.optionals cudaSupport [
+    (lib.cmakeFeature "PBRT_OPTIX_PATH" "${optix}")
+    (lib.cmakeFeature "PBRT_GPU_SHADER_MODEL" "${builtins.head cudaPackages.flags.realArches}")
+    (lib.cmakeFeature "PBRT_CUDA_LIB" "${lib.getOutput "stubs" cudaPackages.cuda_cudart}/lib/stubs/libcuda.so")
+  ];
+
+  postFixup = lib.optionalString cudaSupport ''
+    addDriverRunpath $out/bin/pbrt
+  '';
+
+  meta = {
+    description = "Source code to pbrt, the ray tracer described in the forthcoming 4th edition of the \"Physically Based Rendering: From Theory to Implementation\" book.";
+    homepage = "github.com/mmp/pbrt-v4";
+    mainProgram = "pbrt";
+    license = with lib.licenses; [ asl20 ] ++ lib.optional cudaSupport nvidiaCudaRedist;
+    platforms = with lib.platforms; linux ++ darwin;
+    maintainers = [ lib.maintainers.tsssni ];
+  };
+}
